7c3a02da7efb00a88f79b7ed29a958472502af66
VladislavCh <chupakhin.vladislav@gmail.com>
2021-04-14 23:47:22 +0200

    Хотел сделать меню, но времени на это нет. как есть.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private void clickSend(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText()+"\n");
+        textField.clear();
+        textField.requestFocus();
+
+    }
+
+    @FXML
+    private void clickClose(ActionEvent actionEvent) {
+
+    }
+
+    public void clickClear(ActionEvent actionEvent) {
+        textField.clear();
+        textArea.clear();
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("чат");
+        primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
